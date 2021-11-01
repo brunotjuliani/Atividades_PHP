1409f398aa95c39e7e4108ab2a1e624f809ccaa8
Bruno Juliani <brunotjuliani@hotmail.com>
2021-11-01 12:38:44 -0300

    Secao 13

--- dao/index.php
+++ dao/index.php
@@ -5,10 +5,43 @@ error_reporting(E_ALL);
 
 require_once("config.php");
 
-$sql = new Sql();
+// $sql = new Sql();
+// $usuarios = $sql -> select("SELECT * FROM tb_usuarios");
+// echo json_encode($usuarios)
 
-$usuarios = $sql -> select("SELECT * FROM tb_usuarios");
+// // CARREGA UM USUARIO
+// $user = new Usuario();
+// $user -> loadbyId(3);
+// echo $user
 
-echo json_encode($usuarios);
+// // CARREGA UMA LISTA DE USUARIOS
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+// // CARREGA UMA LISTA DE USUARIOS BUSCANDO PELO LOGIN
+// $busca = Usuario::search("se");
+// echo json_encode($busca);
+
+// // VALIDA UM USUARIO COM LOGIN E SENHA
+// $usuario = new Usuario();
+// $usuario->login("root", "abcde");
+// echo $usuario;
+
+// // INSERE UM NOVO USUARIO
+// $aluno = new Usuario("aluno3", "12345");
+// $aluno->inserir();
+// echo $aluno;
+
+// // ATUALIZANDO DADOS DE UM USUARIO
+// $usuario = new Usuario();
+// $usuario->loadById(8);
+// $usuario->atualizar("professor", "bola1");
+// echo $usuario;
+
+// DELETANDO UM USUARIO
+$usuario = new Usuario();
+$usuario->loadById(7);
+$usuario->deletar();
+echo $usuario;
 
 ?>
