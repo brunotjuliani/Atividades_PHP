b1083d1cc8ab8066550c631aac81ad95c29b978b
Bruno Juliani <brunotjuliani@hotmail.com>
2021-11-01 17:37:11 -0300

    Secao 14 pt 1

---
+++ dir/classes/Sql.php
@@ -0,0 +1,38 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class Sql extends PDO {
+
+    private $conn;
+
+    public function __construct(){
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp", "root", "");
+    }
+
+    private function setParams($statement, $parameters = array()){
+        foreach ($parameters as $key => $value){
+            $this->setParam($statement, $key, $value);
+        }
+    }
+
+    private function setParam($statement, $key, $value){
+        $statement->bindParam($key, $value);
+    }
+
+    public function execQuery($rawQuery, $params = array()){
+        $stmt = $this->conn->prepare($rawQuery);
+        $this->setParams($stmt, $params);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function select($rawQuery, $params = array()):array{
+        $stmt = $this->execQuery($rawQuery, $params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+}
+
+?>
